7d247bb21be218abb204c6acbe7044a59100863a
Alberto Almenara <alberto.almenara@gmail.com>
2023-05-05 15:06:28 +0200

    tablero dibujado dinamicamente a partir las opciones de la partida

--- src/app/modules/game/game/game.component.ts
+++ src/app/modules/game/game/game.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { game } from 'src/app/models/game.model';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
+  @Input() game!:game
+
+  constructor(){
+  }
+  
+  ngOnInit() {
+    console.log(this.game)
+  }
 }
