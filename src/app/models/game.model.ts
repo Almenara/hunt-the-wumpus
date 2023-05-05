4fabd82b02a9df9d71647ac337ef12bf2c98e791
Alberto Almenara <alberto.almenara@gmail.com>
2023-05-05 14:10:53 +0200

    estructura primitiva de tablero definida y generada con los datos de configuracion de partida

---
+++ src/app/models/game.model.ts
@@ -0,0 +1,29 @@
+export class game{
+    cells   : number;
+    holes   : number;
+    arrows  : number;
+
+    board  : number[][] = [];   
+
+    constructor(data: any){
+        this.cells  = data.cells;
+        this.holes  = data.holes;
+        this.arrows = data.arrows;
+        this.createBoard()
+    }
+
+    createBoard(){
+        for(let i = 0; i < this.cells; i++){
+
+            let row: number[] = new Array;
+
+            for(let e:number = 1; e < this.cells +1 ; e++){
+                row.push(e+(i * this.cells));
+            }
+
+            this.board.push(row);
+            
+        }
+        console.log(this.board)
+    }
+}
